9abae9525a74d2fdf8ec377499d539cc2e5061cf
unknown <smruthibs.xworkz@gmail.com>
2022-09-18 18:49:59 +0530

    19th sep

---
+++ programs/PersonRunner.java
@@ -0,0 +1,18 @@
+class PersonRunner{
+public static void main(String[] people){
+	Person.work("wipro",30000);
+	Person.work("wipro",30000," technical trainee");
+	Person.work("capgemini",35000);
+	Person.work("capgemini",35000,"web developer");
+	Person.work("TCS",30000);
+	Person.work("TCS",30000,"system engineer");
+	Person.work("IBM",450000);
+	Person.work("IBM",450000,"HR");
+	Person.work("NTT data",250000);
+	Person.work("NTT data",250000,"software engineer");
+	
+	
+	
+	
+}
+}
